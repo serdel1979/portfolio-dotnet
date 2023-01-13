877c4e3bc54c2869564de0ec2b56f78e8910f84a
Sergio De Luca <sdlbsso@gmail.com>
2023-01-13 19:08:47 -0300

    Inicio de portfolio

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Portfolio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index","Sergio De Luca");
         }
 
         public IActionResult Privacy()
